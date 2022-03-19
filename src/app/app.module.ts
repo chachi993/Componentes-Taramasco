83aef57cfe421ce90f3b9f5cb4288d583e369a33
Sabrina Taramasco <sabrinataramasco@hotmail.com>
2022-03-19 19:00:58 -0300

    Avance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SideMenuComponent } from './components/sidemenu/sidemenu.component';
+import { ContentComponent } from './components/content/content.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SideMenuComponent,
+    ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,
+    NavbarComponent,
+    SideMenuComponent,
+    ContentComponent,]
 })
 export class AppModule { }
